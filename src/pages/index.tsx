375186b29ccae7fbacb3d97fa4a4382b1aa0fc64
cyu60 <chinatchinat123@gmail.com>
2023-02-11 21:35:45 -0500

    add login page and sign up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,14 +48,60 @@ const Home: NextPage = () => {
               // target="_blank"
             >
               <h3 className="text-2xl font-bold">Go to Chinat page →</h3>
-              <div className="text-lg">
-                Go to Chinat page.
-              </div>
+              <div className="text-lg">Go to Chinat page.</div>
             </Link>
           </div>
+
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
+        </div>
+        {/* </div> */}
+        <div className="min-w-screen hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Login now!</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+            </div>
+            <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
+              <div className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    className="input-bordered input"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="password"
+                    className="input-bordered input"
+                  />
+                  <label className="label">
+                    <a href="#" className="link-hover label-text-alt link">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                  <Link href="login">
+                  <button className="btn-primary btn">Login</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
---
+++ src/pages/login.tsx
@@ -0,0 +1,33 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "../utils/api";
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn-primary btn">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
