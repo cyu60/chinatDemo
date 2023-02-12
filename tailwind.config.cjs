60249af6949f1d51eef763541deba0c432c6ec7a
cyu60 <chinatchinat123@gmail.com>
2023-02-11 21:39:45 -0500

    update daisy ui"

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["node_modules/daisyui/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
