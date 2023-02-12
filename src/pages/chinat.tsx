9684830f95345b88ee6054ac5f7314ddc7e2e854
cyu60 <chinatchinat123@gmail.com>
2023-02-11 21:09:46 -0500

    new pages

---
+++ src/pages/chinat.tsx
@@ -0,0 +1,21 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "../utils/api";
+
+const Chinat: NextPage = () => {
+
+  return (
+    <>
+      <main className="">
+        <h1>This is chinat's page</h1>
+      </main>
+      <Link href="/" className="flex max-w-xs text-blue-600 flex-col gap-4 rounded-xl p-4 hover:bg-white/20">
+        <p>Go to homepage</p>
+      </Link>
+    </>
+  );
+};
+
+export default Chinat;
